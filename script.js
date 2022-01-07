95946742b919fb804208504c5a20190592125283
Chris41700 <ChrisHui417@gmail.com>
2022-01-06 23:37:11 -0500

    Question Two Complete

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-//Question 1
-const leftBox = document.querySelector('.left')
-const rightBox = document.querySelector('.right')
-const box = document.querySelector('.box')
+//Christopher Hui
+//TTP Assignment-4
 
-rightBox.onclick = () => box.textContent = "I'm right";
-leftBox.onclick = () => box.textContent = "No, I'm right";
+//Question 1
+// const leftBox = document.querySelector('.left')
+// const rightBox = document.querySelector('.right')
+// const box = document.querySelector('.box')
+
+// rightBox.onclick = () => box.textContent = "I'm right";
+// leftBox.onclick = () => box.textContent = "No, I'm right";
+
+//Question 2
+// const hover = document.querySelector('div');
+
+// document.addEventListener("mouseover", e => {
+//     alert("Hey, I told you not to hover over me!");
+// })
+
+//Question 3
