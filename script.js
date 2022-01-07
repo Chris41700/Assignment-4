596eff92bde30ce5f4bc2eb0fd0ae5fd8af84378
Chris41700 <ChrisHui417@gmail.com>
2022-01-07 04:00:27 -0500

    Question 4 & 5 Complete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Christopher Hui
 //TTP Assignment-4
+//Please comment every other question when testing one
 
 //Question 1
 // const leftBox = document.querySelector('.left')
@@ -16,3 +17,27 @@
 //     alert("Hey, I told you not to hover over me!");
 // })
 
+//Question 4
+const username = document.getElementById('name');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+const submission = document.querySelector('.submitted');
+let numbers = [9];
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+
+    if (password.value.match(numbers)) {
+        alert("Password must be 012345678");
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+})
+
+// form.onclick = function validInformation() {
+//     submission.textContent = "Submission Completed!";
+// }
